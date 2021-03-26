751ccfed8db01af98da6fe10da34ca5a19fa0663
Jan Müller <janmueller3698@gmail.com>
2021-03-26 13:31:30 +0100

    refactor: remove redundant code

--- model/link.ts
+++ model/link.ts
@@ -4,21 +4,12 @@ import { D3Node } from '~/model/node'
 export interface D3Link extends SimulationLinkDatum<D3Node> {
   source: D3Node
   target: D3Node
-  relations: Set<string>
-  functions: Set<string>
 }
 
 export class Link implements D3Link {
-  public readonly relations = new Set<string>()
-  public readonly functions = new Set<string>()
-
+  // eslint-disable-next-line no-useless-constructor
   public constructor(
     public readonly source: D3Node,
-    public readonly target: D3Node,
-    initialRelations?: string[],
-    initialFunctions?: string[]
-  ) {
-    initialRelations?.forEach((relation) => this.relations.add(relation))
-    initialFunctions?.forEach((functions) => this.functions.add(functions))
-  }
+    public readonly target: D3Node
+  ) {}
 }
